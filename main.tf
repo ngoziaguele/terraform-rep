c0d56a95b79c6fadfecb8bd89cc0494532abb2bd
Ngozi Aguele <ngozi@xDesigns-MacBook-Pro.local>
2023-07-17 16:11:22 +0100

    nat-gateway

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "tuesdayigw" {
 
 
 resource "aws_subnet" "tuesdaypublicsubnet" {
-    count = 2
+    count = 3
     vpc_id = aws_vpc.tuesday-learning.id
     cidr_block = element((var.cidrs), count.index)
     availability_zone = element((var.az), count.index)
@@ -30,17 +30,19 @@ resource "aws_subnet" "tuesdaypublicsubnet" {
 }
 
 resource "aws_subnet" "tuesdayprivatesubnet" {
+    count = 3
+
     vpc_id = aws_vpc.tuesday-learning.id
-    availability_zone = "eu-west-2c"
+    availability_zone = element((var.az), count.index)
+    cidr_block = element((var.privatecidrs), count.index)
     
-    cidr_block = "192.168.0.16/28"
     tags = {
-        Name = "tuesdayprivatesubnet"
+        Name = "${element(var.private_subnet, count.index)}"
     }
 }
 
 resource "aws_route_table" "tuesdaypubroute" {
-    count = 2
+    count = 3
     vpc_id = aws_vpc.tuesday-learning.id
     
     tags = {
@@ -50,7 +52,7 @@ resource "aws_route_table" "tuesdaypubroute" {
 }
 
 resource "aws_route" "main" {
-    count = 2
+    count = 3
     destination_cidr_block = "0.0.0.0/0"
     route_table_id = element(aws_route_table.tuesdaypubroute.*.id, count.index)
     gateway_id = aws_internet_gateway.tuesdayigw.id
@@ -58,7 +60,7 @@ resource "aws_route" "main" {
 
 
 resource "aws_route_table_association" "pubassociation" {
-    count = 2
+    count = 3
     subnet_id = element((aws_subnet.tuesdaypublicsubnet.*.id), count.index)
     route_table_id = element(aws_route_table.tuesdaypubroute.*.id, count.index)
 }
@@ -68,7 +70,7 @@ resource "aws_security_group" "tuesdaysg" {
     description = "Allow TLS inbound traffic"
     vpc_id = aws_vpc.tuesday-learning.id
 
-    ingress{
+    ingress {
         description = "HTTP from VPC"
         from_port   = 80
         to_port = 80
@@ -76,9 +78,35 @@ resource "aws_security_group" "tuesdaysg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        description = "HTTPS from VPC"
+        from_port   = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "SSH from VPC"
+        from_port   = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        description = "access to the internet"
+        from_port  = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
+
+
 resource "aws_instance" "tuesday-learning" {
-    count = 2
+    count = 3
     ami = "ami-0eb260c4d5475b901"
     key_name = "class2"
     instance_type = "t2.micro"
@@ -93,3 +121,28 @@ resource "aws_instance" "tuesday-learning" {
         Name = "${element(var.public_subnet, count.index)}-instance"
     }
 }
+
+resource "aws_eip" "lb"  {
+    count = 3
+    vpc = true
+}
+
+resource "aws_nat_gateway" "main" {
+    count = 3
+    allocation_id = element(aws_eip.lb.*.id, count.index)
+    subnet_id = element(aws_subnet.tuesdaypublicsubnet.*.id, count.index)
+tags = {
+   Name = "${element(var.nat_gateway, count.index)}" 
+}
+
+
+}
+
+# resource "aws_lb_target_group" "test" {
+#     name = "tf-tuesdaytargetgroup-lb-tg"
+#     port = 80
+#     protocol = "HTTP"
+#     vpc_id = aws_vpc.tuesday-learning.id
+# }
+
+
--- vars.tf
+++ vars.tf
@@ -1,18 +1,18 @@
 variable "az" {
     description = "avalability zone"
-    default = ["eu-west-2a", "eu-west-2b"]
+    default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
     type = list(string)
 }
 
 variable "route-names" {
     description = "route names"
-    default = ["public1", "public2"]
+    default = ["public1", "public2", "public3"]
     type = list(string)
 }
 
 variable "cidrs" {
     description = "cidr block"
-    default = ["192.168.0.0/28", "192.168.0.32/28"]
+    default = ["192.168.0.0/28","192.168.0.16/28", "192.168.0.32/28"]
     type = list(string)
 }
 
@@ -24,6 +24,29 @@ variable "vpc_cidr" {
 }
 variable "public_subnet" {
     description = "public subnets"
-    default = ["public-subnet1", "public-subnet2"]
+    default = ["public-subnet1", "public-subnet2","public-subnet3"]
+    type = list(string)
+}
+
+
+variable "private_subnet" {
+    description = "private subnets"
+    default = ["private-subnet1", "private-subnet2", "private-subnet3"]
+    type = list(string)
+}    
+
+variable "privatecidrs" {
+    description = "private cidr block"
+    default = ["192.168.0.48/28", "192.168.0.64/28", "192.168.0.80/28"]
+    type = list(string)
+}
+variable "elasticip" {
+    description = "elastic ip"
+
+}
+
+variable "nat_gateway" {
+    description = "nat gateway"
+    default = ["nat-gateway1", "nat-gateway2", "nat-gateway3"]
     type = list(string)
 }
